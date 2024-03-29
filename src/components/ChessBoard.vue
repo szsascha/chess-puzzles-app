1a01c5a1566ad5a74ead376463a0b59a4640ac8a
szsascha <43013380+szsascha@users.noreply.github.com>
2024-03-29 22:04:55 +0100

    Add movement markers

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -3,24 +3,32 @@
         id="chessBoard"
         fen="start"
         >
+      <div
+        v-for="marker in markers"
+        :key="marker.class"
+        :class="marker.class"
+        :slot="marker.slot"
+        ></div>
     </g-chess-board>
     <ion-button expand="full" @click="onNextPuzzle">Next Puzzle</ion-button>
     <ion-text color="white" id="result"><center>Take your turn</center></ion-text>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import 'gchessboard'
 import { Chess } from 'chess.js'
 import ChessService from '../services/ChessService'
 
 const game = new Chess();
+const markers: [{class: string, slot: string}] | any = ref()
 const chessService = new ChessService();const onNextPuzzle = () => chessService.nextPuzzle()
     .then(fen => {
         const board = document.getElementById("chessBoard") as GChessBoardElement;
         board.fen = fen
         game.load(fen)
         board.interactive = true
+        markers.value = null
     })
     .catch(error => console.error("Error fetching puzzle:", error))
 
@@ -53,8 +61,19 @@ onMounted(() => {
             if (expectedMove == chessService.bestMove) {
                 console.log('success');
                 resultElement.innerHTML = 'Correct move!';
+                markers.value = [
+                    { class: 'green-marker', slot: e.detail.from},
+                    { class: 'green-marker', slot:  e.detail.to}
+                ]
             } else {
                 resultElement.innerHTML = 'Wrong move - ' + expectedMove + ' was expected.';
+                
+                markers.value = [
+                    { class: 'green-marker', slot: chessService.bestMove.substring(0, 2)},
+                    { class: 'green-marker', slot:  chessService.bestMove.substring(2)},
+                    { class: 'red-marker', slot: e.detail.from},
+                    { class: 'red-marker', slot:  e.detail.to}
+                ]
             }
         } else {
             console.error("result element is null")
@@ -74,5 +93,17 @@ ion-button {
     margin: auto;
     max-width: 500px;
     --background: #4c9469;
+}
+
+.red-marker {
+  background-color: rgb(206, 52, 52);
+  width: 100%;
+  height: 100%;
+}
+
+.green-marker {
+  background-color: rgb(32, 129, 32);
+  width: 100%;
+  height: 100%;
 }
 </style>
