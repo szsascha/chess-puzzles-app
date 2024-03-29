e3673b82339452319b204010123a595abb055728
szsascha <43013380+szsascha@users.noreply.github.com>
2024-03-29 17:46:38 +0100

    Initial setup with chessboard

---
+++ src/components/ChessBoard.vue
@@ -0,0 +1,18 @@
+<template>
+    <g-chess-board 
+        fen="start"
+        interactive="true"
+        >
+    </g-chess-board>
+</template>
+  
+<script setup lang="ts">
+import "gchessboard";
+</script>
+  
+<style scoped>
+g-chess-board {
+    margin: auto;
+    max-width: 500px;
+}
+</style>
